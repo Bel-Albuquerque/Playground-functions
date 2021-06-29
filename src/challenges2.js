68c0e69a4632c58541705a15aa818606285bec52
Bel Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-06-29 18:43:46 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,9 +19,56 @@ return box
 console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'bel'))
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function highestCount (array){
+  let conferindo=array
+  let contador=0
+  for (let index=0; index<array.length; index+=1){
+    for (let confIndex=0; confIndex<conferindo.length; confIndex+=1){
+      if (array[index] === conferindo[confIndex]){
+      contador+=1
+      }
+      if (contador>=3){
+        return contador
+        break
+      }
+
+    }
+    contador=0
+  }
+  return contador
 }
+
+
+
+
+function generatePhoneNumber(array) {
+  if (array.length != 11) {
+    return "Array com tamanho incorreto.";
+  } 
+  for (let index=0; index<array.length; index += 1) {
+    if (array[index]<0 || array[index]>9 ||highestCount (array)===3){
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+  }
+  let prefixo ='('
+  for (let index=0; index<2; index+=1){
+      prefixo+= array[index]    
+  }
+  let tel1= prefixo + ') '
+  for (let index=2; index<7; index+=1){
+      tel1+=array[index]
+  }
+  let telFinal= tel1 + '-'
+  for (let index=7; index<array.length; index++){
+      telFinal+=array[index]
+  }
+  
+      return telFinal
+}
+  
+  
+ 
+  
 
 // Desafio 12
 function triangleCheck() {
