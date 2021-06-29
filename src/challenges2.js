fb6e39350bda36bd1be0f6abf3b8948fb5eb4e24
Bel Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-06-29 19:45:22 -0300

    Desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -71,9 +71,18 @@ function generatePhoneNumber(array) {
   
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if( lineA < lineB+lineC && lineA > Math.abs(lineB-lineC) &&
+  lineB < lineA+lineC && lineB > Math.abs(lineA-lineC) &&
+  lineC < lineB+lineA && lineC > Math.abs(lineB-lineA)){
+    return true
+  } else {
+    return false
+  }
+
 }
+
+console.log(triangleCheck(10, 14, 8))
 
 // Desafio 13
 function hydrate(string) {
